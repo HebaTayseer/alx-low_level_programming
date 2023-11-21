e762e27d57bfe0a7ad8d03fb8ee1e24d7d2b60dc
HebaTayseer <63527596+HebaTayseer@users.noreply.github.com>
2023-11-21 09:50:32 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,30 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-/**
- * strtow - concatenates arguments.
- * @str: String to be splitted.
- *
- * Return: a pointer to array of String.
- */
-char **strtow(char *str)
-{
-	char *array = NULL;
-	unsigned int i = 0, j = 0, k;
 
-	if (strncmp(str, "", 1) || str == NULL)
-		return (NULL);
-	array = malloc((i + j + 1) * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		array[k] = str[k];
-	i = k;
-	for (k = 0; k < j; k++)
-	{
-		array[i] = str[k];
-		i++;
-	}
-	array[i] = '\0';
-	return (NULL);
+
+/**
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: The argument count.
+ * @av: The argument vector.
+ *
+ * Return: A pointer to the new string, or NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+    int i, j, k, len;
+    char *str;
+
+    if (ac == 0 || av == NULL)
+        return (NULL);
+
+    len = 0;
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+            len++;
+        len++; /* Add space for newline character */
+    }
+
+    str = malloc((len + 1) * sizeof(char));
+    if (str == NULL)
+        return (NULL);
+
+    k = 0;
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+        {
+            str[k] = av[i][j];
+            k++;
+        }
+        str[k] = '\n'; /* Add newline character after each argument */
+        k++;
+    }
+
+    str[k] = '\0'; /* Null-terminate the string */
+
+    return (str);
 }
