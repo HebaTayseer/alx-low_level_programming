5081ff5a019aa8eae15979db042363654370856e
HebaTayseer <63527596+HebaTayseer@users.noreply.github.com>
2023-11-21 09:52:40 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,37 +13,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, k, len;
-    char *str;
+	int i, j, k, len;
+	char *str;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            len++;
-        len++; /* Add space for newline character */
-    }
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++; /* Add space for newline character */
+	}
 
-    str = malloc((len + 1) * sizeof(char));
-    if (str == NULL)
-        return (NULL);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
 
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            str[k] = av[i][j];
-            k++;
-        }
-        str[k] = '\n'; /* Add newline character after each argument */
-        k++;
-    }
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n'; /* Add newline character after each argument */
+		k++;
+	}
 
-    str[k] = '\0'; /* Null-terminate the string */
+	str[k] = '\0'; /* Null-terminate the string */
 
-    return (str);
+	return (str);
 }
