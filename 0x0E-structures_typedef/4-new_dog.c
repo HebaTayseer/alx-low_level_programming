19647acfc37a75ee0a736e5430d1d957d072eb51
HebaTayseer <63527596+HebaTayseer@users.noreply.github.com>
2023-11-28 09:46:48 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,39 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+
 /**
- * init_dog - cinitialize a dog data structure.
- * @d: dog pointer.
- * @name: dog's name.
- * @age: dog's age.
- * @owner: dog's owner
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
+ * Return: Pointer to the new dog, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *d;
+    dog_t *new_dog;
 
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	return (d);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+
+    new_dog->name = strdup(name);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+
+    new_dog->age = age;
+
+    new_dog->owner = strdup(owner);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
+
+    return (new_dog);
 }
