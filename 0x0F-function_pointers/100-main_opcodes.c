620875db19576dc1ae7f9af9b95df8745699ac4a
HebaTayseer <63527596+HebaTayseer@users.noreply.github.com>
2023-11-30 11:45:31 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,23 +3,39 @@
 #include "function_pointers.h"
 
 /**
- * main - prints a name as is
- * @argc: name of the person
- * @argv: arguments vector,
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Nothing.
+ * Return: 0 on success, 1 on incorrect number of arguments, 2 on negative number of bytes
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
+
+	char *main_ptr = (char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i] & 0xFF);
+
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
 	return (0);
 }
+
