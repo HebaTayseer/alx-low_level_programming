c0d23093847cfc15f56842599e39f41cb2e137ab
HebaTayseer <63527596+HebaTayseer@users.noreply.github.com>
2023-12-18 19:16:29 +0200

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,48 +3,49 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+#include "main.h"
 
 /**
  * print_elf_header - Print information from the ELF header.
  * @header: Pointer to the ELF header structure.
  */
 void print_elf_header(Elf64_Ehdr *header) {
-    printf("ELF Header:\n");
-    printf("  Magic:   ");
+    _printf("ELF Header:\n");
+    _printf("  Magic:   ");
     for (int i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", header->e_ident[i]);
+        _printf("%02x ", header->e_ident[i]);
     }
-    printf("\n");
-    printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-    printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-    printf("  OS/ABI:                            ");
+    _printf("\n");
+    _printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+    _printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+    _printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    _printf("  OS/ABI:                            ");
     switch (header->e_ident[EI_OSABI]) {
         case ELFOSABI_SYSV:
-            printf("UNIX - System V\n");
+            _printf("UNIX - System V\n");
             break;
         case ELFOSABI_NETBSD:
-            printf("UNIX - NetBSD\n");
+            _printf("UNIX - NetBSD\n");
             break;
         case ELFOSABI_SOLARIS:
-            printf("UNIX - Solaris\n");
+            _printf("UNIX - Solaris\n");
             break;
         default:
-            printf("<unknown: %d>\n", header->e_ident[EI_OSABI]);
+            _printf("<unknown: %d>\n", header->e_ident[EI_OSABI]);
     }
-    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-    printf("  Type:                              ");
+    _printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    _printf("  Type:                              ");
     switch (header->e_type) {
         case ET_EXEC:
-            printf("EXEC (Executable file)\n");
+            _printf("EXEC (Executable file)\n");
             break;
         case ET_DYN:
-            printf("DYN (Shared object file)\n");
+            _printf("DYN (Shared object file)\n");
             break;
         default:
-            printf("<unknown: %d>\n", header->e_type);
+            _printf("<unknown: %d>\n", header->e_type);
     }
-    printf("  Entry point address:               0x%lx\n", header->e_entry);
+    _printf("  Entry point address:               0x%lx\n", header->e_entry);
 }
 
 /**
@@ -55,21 +56,21 @@ void print_elf_header(Elf64_Ehdr *header) {
  */
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-        exit(98);
+        _printf("Usage: %s elf_filename\n", argv[0]);
+        return 98;
     }
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
-        dprintf(STDERR_FILENO, "Error: Cannot open file %s\n", argv[1]);
-        exit(98);
+        _printf("Error: Cannot open file %s\n", argv[1]);
+        return 98;
     }
 
     Elf64_Ehdr elf_header;
     if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header)) {
-        dprintf(STDERR_FILENO, "Error: Cannot read ELF header from file %s\n", argv[1]);
+        _printf("Error: Cannot read ELF header from file %s\n", argv[1]);
         close(fd);
-        exit(98);
+        return 98;
     }
 
     print_elf_header(&elf_header);
